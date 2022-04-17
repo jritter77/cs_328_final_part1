85661894efd2d4b44a6c474263539de481add025
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 15:38:19 -0700

    - testing

--- server/submission.php
+++ server/submission.php
@@ -27,25 +27,7 @@ while(oci_fetch($empl_query_stmt)) {
     echo oci_result($empl_query_stmt, 'HIREDATE');
 }
 
-$props_query_str = '
-
-    drop table props;
-
-    create table props(
-        seller_name varchar(30),
-        seller_phone varchar(12),
-        seller_address varchar(50),
-        building_name varchar(30),
-        building_price number,
-        building_address varchar(50),
-        area varchar(20),
-        type varchar(20),
-        sqft number,
-        height number,
-        rooms number,
-        floors number,
-        year_built number
-    );';
+$props_query_str = 'select * from empl';
 
 
 $props_query_stmt = oci_parse($conn, $props_query_str);
