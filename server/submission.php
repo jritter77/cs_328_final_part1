d99eb9bf4a0c667150b76b6c2cecff23c07f05ab
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 15:12:45 -0700

    - testing

--- server/submission.php
+++ server/submission.php
@@ -1,9 +1,13 @@
 <?php
 
-// get request params
-$req = json_decode($_POST['req']);
 
-$conn = oci_connect("jr550", "Wo0dabu9a");
+$db_conn_str =
+            "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)
+                                       (HOST = cedar.humboldt.edu)
+                                       (PORT = 1521))
+                            (CONNECT_DATA = (SID = STUDENT)))";
+
+$conn = oci_connect("jr550", "Wo0dabu9a", $db_conn_str);
 
 if (!$conn) {
     echo 'Error connecting...';
