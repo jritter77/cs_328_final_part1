e8ed7caf78b3a21d25b9585be8a1b93aafc19ff4
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 15:55:41 -0700

    - testing

--- server/submission.php
+++ server/submission.php
@@ -69,6 +69,50 @@ oci_free_statement($props_query_stmt);
 echo 'Table Created!';
 
 
+$props_query_str = "
+insert into props 
+values (
+    'John Smith',
+    '555-555-5555',
+    '123 County Ln, Eureka CA 95501',
+    'Old Victorian',
+    120000,
+    '123 County Ln, Eureka CA 95501',
+    'Eureka',
+    'Residential',
+    1500,
+    50,
+    3,
+    2,
+    1937
+)
+";
+
+
+$props_query_stmt = oci_parse($conn, $props_query_str);
+
+oci_execute($props_query_stmt, OCI_DEFAULT);
+
+
+
+$props_query_str = '
+        select * from props
+';
+
+
+$props_query_stmt = oci_parse($conn, $props_query_str);
+
+oci_execute($props_query_stmt, OCI_DEFAULT);
+
+while (oci_fetch($props_query_stmt)) {
+    echo oci_result($props_query_stmt, 'SELLER_NAME');
+}
+
+oci_free_statement($props_query_stmt);
+
+echo 'Table Created!';
+
+
 
 
 oci_free_statement($empl_query_stmt);
