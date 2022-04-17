088e4e85830be8ced6382a65205ea047e0c19f7a
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 15:07:52 -0700

    - testing connection

--- server/submission.php
+++ server/submission.php
@@ -3,9 +3,16 @@
 // get request params
 $req = json_decode($_POST['req']);
 
-foreach ($req as $col=>$val) {
-    echo $col . ' ' . $val;
+$conn = oci_connect("jr550", "Wo0dabu9a");
+
+if (!$conn) {
+    echo 'Error connecting...';
 }
+else {
+    echo 'Success!'
+}
+
+
 
 
 
