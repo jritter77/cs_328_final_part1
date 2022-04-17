72f94a7e6f63df79f91b0061f6a4ed0fcca2d341
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 15:27:51 -0700

    - testing

--- server/submission.php
+++ server/submission.php
@@ -18,23 +18,8 @@ else {
 
 $props_query_str = '
 
-    drop table props;
-
-    create table props(
-        seller_name varchar(30),
-        seller_phone varchar(12),
-        seller_address varchar(50),
-        building_name varchar(30),
-        building_price number,
-        building_address varchar(50),
-        area varchar(20),
-        type varchar(20),
-        sqft number,
-        height number,
-        rooms number,
-        floors number,
-        year_built number
-    );';
+    select * from empl
+    ;';
 
 
 $props_query_stmt = oci_parse($conn, $props_query_str);
