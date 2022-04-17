f955b9ad0bf57320e91ccff753581464bebc4196
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 15:37:16 -0700

    - testing

--- server/submission.php
+++ server/submission.php
@@ -54,83 +54,9 @@ oci_execute($props_query_stmt, OCI_DEFAULT);
 
 echo 'Table Created!';
 
-$props_query_str = "
-
-insert into props 
-values (
-    'John Smith',
-    '555-555-5555',
-    '123 County Ln, Eureka CA 95501',
-    'Old Victorian',
-    120000,
-    '123 County Ln, Eureka CA 95501',
-    'Eureka',
-    'Residential',
-    1500,
-    50,
-    3,
-    2,
-    1937
-);
 
 
-insert into props 
-values (
-    'Jane Doe',
-    '555-555-5555',
-    '555 Alley way, Eureka CA 95501',
-    'Fixer Upper',
-    80000,
-    '555 Alley way, Eureka CA 95501',
-    'Eureka',
-    'Residential',
-    800,
-    30,
-    2,
-    1,
-    1962
-);
-
-
-insert into props 
-values (
-    'John Smith',
-    '555-555-5555',
-    '123 County Ln, Eureka CA 95501',
-    'Barn',
-    100000,
-    '131 County Ln, Eureka CA 95501',
-    'Eureka',
-    'Commercial',
-    1000,
-    50,
-    0,
-    2,
-    1948
-);";
-
-    
-$props_query_stmt = oci_parse($conn, $props_query_str);
-
-oci_execute($props_query_stmt, OCI_DEFAULT);
-
-echo 'Table Populated!';
-
-
-$props_query_str = '
-    select *
-    from props;
-    ';
-
-
-$props_query_stmt = oci_parse($conn, $props_query_str);
-
-oci_execute($props_query_stmt, OCI_DEFAULT);
-
-while (oci_fetch($props_query_stmt)) {
-    echo oci_result($props_query_stmt, 'SELLER_NAME');
-}
-
+oci_free_statement($empl_query_stmt);
 oci_free_statement($props_query_stmt);
 oci_close($conn);
 
