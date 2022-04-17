959a8da163d1a7d33ecbe26e1ba24279f78495af
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 14:46:55 -0700

    - added db set up files

---
+++ db/create_props_table.sql
@@ -0,0 +1,17 @@
+drop table props;
+
+create table props(
+    seller_name varchar(30),
+    seller_phone varchar(12),
+    seller_address varchar(50),
+    building_name varchar(30),
+    building_price number,
+    building_address varchar(50),
+    area varchar(20),
+    type varchar(20),
+    sqft number,
+    height number,
+    rooms number,
+    floors number,
+    year_built number
+);
---
+++ db/pop_props_table.sql
@@ -0,0 +1,54 @@
+
+
+insert into props 
+values (
+    'John Smith',
+    '555-555-5555',
+    '123 County Ln, Eureka CA 95501',
+    'Old Victorian',
+    120000,
+    '123 County Ln, Eureka CA 95501',
+    'Eureka',
+    'Residential',
+    1500,
+    50,
+    3,
+    2,
+    1937
+);
+
+
+insert into props 
+values (
+    'Jane Doe',
+    '555-555-5555',
+    '555 Alley way, Eureka CA 95501',
+    'Fixer Upper',
+    80000,
+    '555 Alley way, Eureka CA 95501',
+    'Eureka',
+    'Residential',
+    800,
+    30,
+    2,
+    1,
+    1962
+);
+
+
+insert into props 
+values (
+    'John Smith',
+    '555-555-5555',
+    '123 County Ln, Eureka CA 95501',
+    'Barn',
+    100000,
+    '131 County Ln, Eureka CA 95501',
+    'Eureka',
+    'Commercial',
+    1000,
+    50,
+    0,
+    2,
+    1948
+);
