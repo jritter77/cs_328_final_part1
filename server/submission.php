71806ddf7d5ac76f34c4c209b6382209ac422b9f
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 16:03:59 -0700

    - testing

--- server/submission.php
+++ server/submission.php
@@ -19,12 +19,57 @@ else {
 
 
 
+$props_query_str = "
+create table properties(
+    seller_name varchar(30),
+    seller_phone varchar(12),
+    seller_address varchar(50),
+    building_name varchar(30),
+    building_price number,
+    building_address varchar(50),
+    area varchar(20),
+    type varchar(20),
+    sqft number,
+    height number,
+    rooms number,
+    floors number,
+    year_built number
+)
+";
 
+
+$props_query_stmt = oci_parse($conn, $props_query_str);
+
+oci_execute($props_query_stmt, OCI_DEFAULT);
+
+$props_query_str = "
+insert into props 
+values (
+    'Jane Doe',
+    '555-555-5555',
+    '555 Alley way, Eureka CA 95501',
+    'Fixer Upper',
+    80000,
+    '555 Alley way, Eureka CA 95501',
+    'Eureka',
+    'Residential',
+    800,
+    30,
+    2,
+    1,
+    1962
+)
+";
+
+
+$props_query_stmt = oci_parse($conn, $props_query_str);
+
+oci_execute($props_query_stmt, OCI_DEFAULT);
 
 
 
 $props_query_str = '
-        select * from props
+        select * from properties
 ';
 
 
