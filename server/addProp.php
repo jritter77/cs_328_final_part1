2399c7d2223cc3a8b62ee17d688886acbb63ded8
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 20:41:29 -0700

    -testing

--- server/addProp.php
+++ server/addProp.php
@@ -20,7 +20,7 @@ $req = json_decode($_POST['req']);
 $props_query_str = '
         insert into properties
         values (
-            get_date(),
+            curdate(),
             :seller_name,
             :seller_phone,
             :seller_address,
