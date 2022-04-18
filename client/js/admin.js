a93e8e4bfdeede16417cf7d595d0e65b812d4134
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 17:40:02 -0700

    - testing

---
+++ client/js/admin.js
@@ -0,0 +1,11 @@
+import { post } from "./WebRequest.js";
+
+async function getProps() {
+  let result = await get("../../server/getAllProps.php");
+  console.log(result);
+  return JSON.parse(result);
+}
+
+async function populateTable() {}
+
+getProps();
