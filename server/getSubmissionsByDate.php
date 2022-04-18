9507da094d153238b4d2567d64f99389c87b07c2
Jake Ritter <jakeritter77@gmail.com>
2022-04-17 21:06:05 -0700

    - testing

---
+++ server/getSubmissionsByDate.php
@@ -0,0 +1,51 @@
+<?php
+
+
+$db_conn_str =
+            "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)
+                                       (HOST = cedar.humboldt.edu)
+                                       (PORT = 1521))
+                            (CONNECT_DATA = (SID = STUDENT)))";
+
+$conn = oci_connect("jr550", "Wo0dabu9a", $db_conn_str);
+
+if (!$conn) {
+    echo 'Error connecting...';
+}
+
+
+
+$check_date_func = '
+        create or replace function checkDate(start IN date, end IN date) return integer
+        is inPeriod integer;
+
+        begin
+            inPeriod := 1;
+            return inPeriod;
+        end;
+        /
+
+';
+
+
+$check_date_stmt = oci_parse($conn, $check_date_func);
+
+oci_execute($check_date_func, OCI_DEFAULT);
+
+$results = array();
+while ($row = oci_fetch_array($check_date_stmt, OCI_ASSOC)) {
+    array_push($results, $row);
+}
+
+echo json_encode($results);
+
+oci_free_statement($check_date_func);
+
+
+
+
+
+oci_close($conn);
+
+
+?>
