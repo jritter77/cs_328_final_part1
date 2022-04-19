1829fec71a0efa2f615ce338ec80c922ca11b0a1
Jake Ritter <jakeritter77@gmail.com>
2022-04-18 20:57:58 -0700

    -testing

--- server/create_table_proc.php
+++ server/create_table_proc.php
@@ -16,11 +16,20 @@ if (!$conn) {
 
 $create_table_str = "
 
-begin
 
-execute immediate 'drop table properties';
+drop table properties
 
-execute immediate 'create table properties(
+";
+
+$create_table_stmt = oci_parse($conn, $create_table_str);
+
+oci_execute($create_table_stmt, OCI_COMMIT_ON_SUCCESS);
+
+oci_free_statement($create_table_stmt);
+
+$create_table_str = "
+
+create table properties(
     date_submitted date,
     seller_name varchar(30),
     seller_phone varchar(12),
@@ -35,9 +44,8 @@ execute immediate 'create table properties(
     rooms number,
     floors number,
     year_built number
-)';
+)
 
-end;
 ";
 
 $create_table_stmt = oci_parse($conn, $create_table_str);
